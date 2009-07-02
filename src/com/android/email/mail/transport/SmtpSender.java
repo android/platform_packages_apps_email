2e1c230c6c91724f5dee12a9f160148254dc3e43
Marek Pola <marek.pola@gmail.com>
2009-07-02 14:34:55 +0200

    If STARTTLS is on last EHLO response line, accept space prefix.
    When receiving the EHLO response from the SMTP server, the multiline
    answer has "-" prefix in all lines except the last line, where the
    prefix is a blank. This is according to RFC 2821 section 4.2.1. This has
    also been reported as issue 2309 at code.google.com.

--- src/com/android/email/mail/transport/SmtpSender.java
+++ src/com/android/email/mail/transport/SmtpSender.java
@@ -143,7 +143,7 @@ public class SmtpSender extends Sender {
              * if not.
              */
             if (mTransport.canTryTlsSecurity()) {
-                if (result.contains("-STARTTLS")) {
+                if (result.matches("[ -]STARTTLS")) {
                     executeSimpleCommand("STARTTLS");
                     mTransport.reopenTls();
                     /*
