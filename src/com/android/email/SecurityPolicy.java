f47d4e78c31945e6d4a343964b748b1859daab4e
Chris Soyars <ctso@ctso.me>
2010-08-22 01:34:03 -0400

    Fix bug causing the user to be incorrectly prompted to apply security settings.
    Exchange 2007 (by default) requires a minimum password length of 4, this policy
    is still pushed to the device even if the password policy is disabled
    alltogether (mPasswordMode=0).
    
    Change-Id: I127f860b2c8ed75bb35df1b4f6005acf526fe2d4

--- src/com/android/email/SecurityPolicy.java
+++ src/com/android/email/SecurityPolicy.java
@@ -259,7 +259,7 @@ public class SecurityPolicy {
         DevicePolicyManager dpm = getDPM();
         if (dpm.isAdminActive(mAdminName)) {
             // check each policy explicitly
-            if (policies.mMinPasswordLength > 0) {
+            if (policies.mMinPasswordLength > 0 && policies.mPasswordMode > 0) {
                 if (dpm.getPasswordMinimumLength(mAdminName) < policies.mMinPasswordLength) {
                     return false;
                 }
