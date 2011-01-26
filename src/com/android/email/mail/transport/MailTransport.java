861e0a5bc79669d5bc839d157020250e43e1d024
Liu Yen-Liang (Josh) <yenliangl@gmail.com>
2011-01-26 22:02:48 +0800

    Set timeout for blocking read. If connection is lost while reading data from socket, the thread blocks and subsequent commands will never have a chance to run. UI progress will keep spinning for a long time.
    Change-Id: I5a3d096c8eb5d923f58be512670c7aabd2e4faea

--- src/com/android/email/mail/transport/MailTransport.java
+++ src/com/android/email/mail/transport/MailTransport.java
@@ -153,6 +153,7 @@ public class MailTransport implements Transport {
                 mSocket = new Socket();
             }
             mSocket.connect(socketAddress, SOCKET_CONNECT_TIMEOUT);
+            mSocket.setSoTimeout(SOCKET_READ_TIMEOUT);
             mIn = new BufferedInputStream(mSocket.getInputStream(), 1024);
             mOut = new BufferedOutputStream(mSocket.getOutputStream(), 512);
             
