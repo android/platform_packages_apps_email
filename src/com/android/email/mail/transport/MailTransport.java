6d60ed9e9328b52b5c042675280b904a76dddc62
Jun-ya Kato <kato@win6.jp>
2012-03-27 15:56:27 +0900

    Fix connection behavior to a server with multiple IP addrs
    Even if multiple IP addresses of mail server are obtained via DNS,
    Email application only attempts to connect to the fisrt IP address and
    does not try to connect to the second one and later one (if available).
    
    For instance {smtp,imap,pop}.google.com publish two different IP addresses.
    | ;; ANSWER SECTION:
    | smtp.gmail.com.         299     IN      CNAME   gmail-smtp-msa.l.google.com.
    | gmail-smtp-msa.l.google.com. 299 IN     A       173.194.79.108
    | gmail-smtp-msa.l.google.com. 299 IN     A       173.194.79.109
    
    It's better behavior that a TCP client application trys to connect to
    all IP addresses for redundancy and load-balancing from ASP operator's
    view.
    
    Change-Id: Iaa27dce2d4f66e50800fb6c1e542f4e9cc2a01f1
    Signed-off-by: Jun-ya Kato <kato@win6.jp>

--- src/com/android/email/mail/transport/MailTransport.java
+++ src/com/android/email/mail/transport/MailTransport.java
@@ -165,13 +165,12 @@ public class MailTransport implements Transport {
         }
 
         try {
-            SocketAddress socketAddress = new InetSocketAddress(getHost(), getPort());
             if (canTrySslSecurity()) {
-                mSocket = SSLUtils.getSSLSocketFactory(canTrustAllCertificates()).createSocket();
+                mSocket = SSLUtils.getSSLSocketFactory(canTrustAllCertificates()).createSocket(getHost(), getPort());
             } else {
-                mSocket = new Socket();
+                mSocket = new Socket(getHost(), getPort());
             }
-            mSocket.connect(socketAddress, SOCKET_CONNECT_TIMEOUT);
+            mSocket.setSoTimeout(SOCKET_CONNECT_TIMEOUT);
             // After the socket connects to an SSL server, confirm that the hostname is as expected
             if (canTrySslSecurity() && !canTrustAllCertificates()) {
                 verifyHostname(mSocket, getHost());
